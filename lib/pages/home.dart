b1b05f0bf06e3652f38af6cd5f883c8292134348
Edycar Reyes <edycarreyes@gmail.com>
2019-05-04 12:46:12 -0600

    se crea y se configura la pagina del home.dart

---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'HomePage',
+        ),
+      ),
+    );
+  }
+}
